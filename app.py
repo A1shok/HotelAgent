ad57074f60a760825fe1e69f8a02e740848ac0ed
A1shok <namathotiashok@gmail.com>
2026-04-09 08:48:19 +0530

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
+from twilio.twiml.messaging_response import MessagingResponse
 
 app = FastAPI()
 
-@app.get("/")
-def home():
-    return {"status": "running"}
+@app.post("/webhook")
+async def whatsapp_webhook(request: Request):
+    print("🔥 WEBHOOK HIT")
+
+    form = await request.form()
+
+    msg = form.get("Body")
+    phone = form.get("From")
+
+    print("📩 Message:", msg)
+    print("📱 Phone:", phone)
+
+    resp = MessagingResponse()
+    resp.message("FastAPI working 👍")
+
+    return Response(content=str(resp), media_type="application/xml")
