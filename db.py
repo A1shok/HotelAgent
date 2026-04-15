1ec0f06220c944e6a67d647d477556f9ba071599
A1shok <namathotiashok@gmail.com>
2026-04-15 09:27:06 +0530

    db.py

--- db.py
+++ db.py
@@ -6,7 +6,7 @@ import os
 DATABASE_URL = os.getenv("DB_URL")
 
 engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
